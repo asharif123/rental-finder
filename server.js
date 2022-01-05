62ec2199b5973db357e3a8ed792807440a393043
Awad Sharif <awadsharif9@gmail.com>
2022-01-04 21:55:12 -0800

    updated server.js file

--- server.js
+++ server.js
@@ -16,3 +16,30 @@ const helpers = require('./utils/helpers');
 const PORT = process.env.PORT || 3001;
 //require sequelize
 const sequelize = require('sequelize');
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize,
+    }),
+  };
+  
+  app.use(session(sess));
+  
+  const hbs = exphbs.create({ helpers });
+  
+  app.engine('handlebars', hbs.engine);
+  app.set('view engine', 'handlebars');
+
+  app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
