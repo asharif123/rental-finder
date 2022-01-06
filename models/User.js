ede9bea049a23e0a20d4f6e2f2e66058b1a7db73
Mason Newell <mason.newell13@gmail.com>
2022-01-05 21:43:43 -0800

    built user model, created index.js

--- models/User.js
+++ models/User.js
@@ -1,1 +1,54 @@
-//user
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcryptjs');
+const sequelize = require('../config/connection');
+
+class User extends Model {
+  checkPassword(rawPassword) {
+    return bcrypt.compareSync(rawPassword, this.password);
+  }
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [10],
+      },
+    },
+  },
+  {
+    hooks: {
+      beforeCreate(newUser) {
+        newUser.password = bcrypt.hashSync(newUser.password, 10);
+        return newUser;
+      },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  }
+);
+
+module.exports = User;
