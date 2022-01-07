6fb3f70a196b28de03cbfe180a531b7bfff89f0b
Mason Newell <mason.newell13@gmail.com>
2022-01-06 21:54:14 -0800

    Revert "seeds"

--- models/index.js
+++ models/index.js
@@ -1,14 +1,4 @@
-const Favorites  = require("./Favorites");
+// const Rental  = require("./Rental");
 const User = require("./User");
 
-
-    User.hasMany(Favorites, {
-        foreignKey: 'user_id',
-        onDelete: 'CASACADE'
-    });
-
-    Favorites.belongsTo(User, {
-        foreignKey: 'user_id'
-    });
-
-    module.exports = { User, Favorites }
+// User.hasMany(Rental, {
