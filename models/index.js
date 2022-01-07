9856c699ae62857ce1e0dabe5e5194d77c676fc4
Delms <dlelias790@gmail.com>
2022-01-06 21:34:47 -0800

    seeds

--- models/index.js
+++ models/index.js
@@ -1,4 +1,14 @@
-// const Rental  = require("./Rental");
+const Favorites  = require("./Favorites");
 const User = require("./User");
 
-// User.hasMany(Rental, {
+
+    User.hasMany(Favorites, {
+        foreignKey: 'user_id',
+        onDelete: 'CASACADE'
+    });
+
+    Favorites.belongsTo(User, {
+        foreignKey: 'user_id'
+    });
+
+    module.exports = { User, Favorites }
