08de54de9e9c3a367cecc487025e2b5afd155dba
Awad Sharif <awadsharif9@gmail.com>
2022-01-08 13:24:12 -0800

    added logic to signup and login

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const {User} = require('../../models');
+const User = require('../../models');
 
 //Get all users
 router.get('/', async (req, res) =>{
@@ -29,29 +29,63 @@ router.get('/:id', async (req, res) => {
     }
 })
 
-//Create new user
+// CREATE new user
 router.post('/', async (req, res) => {
-    //example post body..
-    /*
-        {
-            "name": "Jane",
-            "email": "Doe",
-            "password": "JaneDonuts",
-            "favRooms": ['{}','{}','{}'] // array of stringified room results from roomsters api
-        }
-    */
-    try
-    {
-        const newUser = await User.create(req.body);
-        console.log("A New User Has Been Successfully created!");
-        res.status(201).json(newUser);
+  try {
+    const dbUserData = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    });
 
-    }catch(err)
-    {
-        console.log(err);
-        res.status(500).send(err);
+    req.session.save(() => {
+      req.session.loggedIn = true;
+
+      res.status(200).json(dbUserData);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// Login
+router.post('/login', async (req, res) => {
+  try {
+    const dbUserData = await User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
+
+    if (!dbUserData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password. Please try again!' });
+      return;
     }
-})
+
+    const validPassword = await dbUserData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password. Please try again!' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.loggedIn = true;
+
+      res
+        .status(200)
+        .json({ user: dbUserData, message: 'You are now logged in!' });
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 //Update user by :id
 router.put('/:id', async (req, res) => {
