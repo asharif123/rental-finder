d96097630f923a975a426c138d0894305f4dd933
Awad Sharif <awadsharif9@gmail.com>
2022-01-13 18:33:32 -0800

    see results when clicking city

--- public/js/search-post.js
+++ public/js/search-post.js
@@ -1,9 +1,9 @@
-async function formHandler(event) {
+async function formHandler(event, searchObj = {} ) {
     event.preventDefault();
-    const city_name = document.querySelector("#city_name").value;
-    const state_name = document.querySelector("#state_name").value;
-    const minimum_budget = document.querySelector("#minimum_budget").value;
-    const maximum_budget = document.querySelector("#maximum_budget").value;
+    const city_name = searchObj.city_name || document.querySelector("#city_name").value;
+    const state_name = searchObj.state_name || document.querySelector("#state_name").value;
+    const minimum_budget = searchObj.minimum_budget || document.querySelector("#minimum_budget").value;
+    const maximum_budget = searchObj.maximum_budget || document.querySelector("#maximum_budget").value;
 // Add new search
 const response = await fetch (`/search`, {
     method: "POST",
@@ -23,5 +23,15 @@ const response = await fetch (`/search`, {
     alert("cannot find any properties");
 }
 }
+
+document.querySelectorAll(".search-history-btn").forEach(node => node.addEventListener("click", (e)=> {
+    const searchObj = {
+        maximum_budget: e.target.dataset.maximum_budget,
+        city_name: e.target.dataset.city_name,
+        state_name: e.target.dataset.state_name,
+        minimum_budget: e.target.dataset.minimum_budget
+    }
+    formHandler(e, searchObj);
+}))
 
 document.querySelector(".search-form").addEventListener("submit", formHandler);
